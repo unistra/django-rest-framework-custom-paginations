7beeb1948530b450e6587b48e9868f6486f92e10
Morgan <morgan.bohn@unistra.fr>
2015-06-02 14:29:10 +0200

    compatibility drf 2.4/3.0

--- rest_framework_custom_paginations/paginations.py
+++ rest_framework_custom_paginations/paginations.py
@@ -1,15 +1,20 @@
-from rest_framework import pagination
-from rest_framework import serializers
+from rest_framework import __version__ as drf_version
+from rest_framework.pagination import PaginationSerializer
 import copy
+if drf_version < '3.0':
+    from rest_framework.serializers import Field as ReadOnlyField
+else:
+    from rest_framework.serializers import ReadOnlyField
 
 
-class SporeNextPageField(serializers.Field):
+
+class SporeNextPageField(ReadOnlyField):
     """
     Field that returns a link to the next page in paginated results.
     """
     page_field = 'page'
 
-    def to_native(self, value):
+    def to_representation(self, value):
         if not value.has_next():
             return None
         page = value.next_page_number()
@@ -18,14 +23,19 @@ class SporeNextPageField(serializers.Field):
         params['page'] = page
         return params
 
+    def to_native(self, value):
+        """
+        DRF 2 compatibility
+        """
+        return self.to_representation(value)
 
-class SporePreviousPageField(serializers.Field):
+class SporePreviousPageField(ReadOnlyField):
     """
     Field that returns a link to the previous page in paginated results.
     """
     page_field = 'page'
 
-    def to_native(self, value):
+    def to_representation(self, value):
         if not value.has_previous():
             return None
         page = value.previous_page_number()
@@ -34,8 +44,14 @@ class SporePreviousPageField(serializers.Field):
         params['page'] = page
         return params
 
+    def to_native(self,value):
+        """
+        DRF 2 compatibility
+        """
+        return self.to_representation(value)
 
-class SporePaginationSerializer(pagination.PaginationSerializer):
+
+class SporePaginationSerializer(PaginationSerializer):
     next_params = SporeNextPageField(source='*')
     previous_params = SporePreviousPageField(source='*')
-    num_pages = serializers.Field(source='paginator.num_pages')
+    num_pages = ReadOnlyField(source='paginator.num_pages')
